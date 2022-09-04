283c403286a1bd9b158f6e83b566bab9dc5fd00c
Tomasz Bazelczuk <tbazelczuk@gmail.com>
2022-09-04 15:02:16 +0200

    test puppeteer

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 const model = require("./model");
 const mailer = require("./mailer");
 const { fetch, fetchAndSave } = require("./repo");
+const { fetchWithPuppeteer } = require("./fetch");
 
 model.connect();
 
@@ -37,6 +38,12 @@ app.put("/api/sites", async (req, res, next) => {
   res.json(site);
 });
 
+app.get("/api/fetch", async (req, res, next) => {
+  const { url, selector } = req.body
+  const value = await fetchWithPuppeteer({ url, selector })
+  res.json(value);
+});
+
 app.put("/api/fetch", async (req, res, next) => {
   const { url, selector } = req.body
   const value = await fetch({ url, selector })
@@ -53,11 +60,6 @@ app.get("/api/sendMail", function (req, res) {
   mailer.sendMail([{ url: "foo", value: 1, prevItem: { value: 2 } }]);
   res.send("sendMail");
 });
-
-// app.get("*", function (req, res) {
-//   res.send("Hello World!");
-//   // res.sendFile(path.join(__dirname, "dist", "crawler", "index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log("Server started at", PORT);
